61f20d12014bd336426387b3ba33c0746d5ee122
rodriguesd <rodriguesd@gmail.com>
2025-03-08 09:02:44 -0800

    add generic json response

--- src/main/java/com/stockheap/weather/controller_advice/WeatherExceptionHandler.java
+++ src/main/java/com/stockheap/weather/controller_advice/WeatherExceptionHandler.java
@@ -7,15 +7,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
-@ControllerAdvice
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class WeatherExceptionHandler {
 
 
 
     @ExceptionHandler(Exception.class)
-    public String handleNotFound(Exception e, Model model) {
-        model.addAttribute("message", "The REST API you are looking for doesn't exist.");
-        return "error";
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An error occurred: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        Map<String, Object> errorResponse = new LinkedHashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+        errorResponse.put("error", "Not Found");
+        errorResponse.put("message", "The requested URL was not found on the server.");
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
--- src/test/java/com/stockheap/weather/functional/WeatherFunctionalTest.java
+++ src/test/java/com/stockheap/weather/functional/WeatherFunctionalTest.java
@@ -1,34 +1,35 @@
 package com.stockheap.weather.functional;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.stockheap.weather.WeatherApplication;
-import com.stockheap.weather.service.external_weather.dto.WeatherDataAndResponseStatusDTO;
-import com.stockheap.weather.service.external_weather.open_weather.response_data.OpenExtendedWeatherResponse;
-import com.stockheap.weather.service.external_weather.open_weather.response_data.Sys;
 import com.stockheap.weather.service.weather.WeatherMethods;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import reactor.core.publisher.Mono;
 
 @SpringBootTest(classes={WeatherApplication.class})
+
+//@WebMvcTest(WeatherController.class) // Specify the controller to test
 public class WeatherFunctionalTest {
 
     @Autowired
     private WeatherMethods weatherMethods;
 
+//    @Autowired
+//    private MockMvc mockMvc;
+
+
     @Test
     public void testCurrentWeather() throws Exception
     {
-        String zip = "94501";
-        String country = "us";
-        Mono<WeatherDataAndResponseStatusDTO> weatherDataAndResponseStatusDTOMono  =  weatherMethods.getCurrentWeather(zip, country);
-        weatherDataAndResponseStatusDTOMono.subscribe(s->{
-
-            String a1 = "";
-
-        });
-        Thread.sleep(2000l);
+//        String zip = "94501";
+//        String country = "us";
+//        Mono<WeatherDataAndResponseStatusDTO> weatherDataAndResponseStatusDTOMono  =  weatherMethods.getExtendedWeather(zip, country);
+//        weatherDataAndResponseStatusDTOMono.subscribe(s->{
+//
+//            String a1 = "";
+//
+//        });
+//        Thread.sleep(2000l);
     }
 }
