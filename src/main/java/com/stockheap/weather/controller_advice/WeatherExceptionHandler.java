ae5178454acc1977b9b965d8ff22293e76625b95
rodriguesd <rodriguesd@gmail.com>
2025-03-07 19:03:05 -0800

    more fixes

---
+++ src/main/java/com/stockheap/weather/controller_advice/WeatherExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.stockheap.weather.controller_advice;
+
+import com.stockheap.weather.controller_advice.response.GenericWeatherResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class WeatherExceptionHandler {
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<GenericWeatherResponse> handleStudentNotFoundException(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new GenericWeatherResponse(exception.getMessage()));
+    }
+}
---
+++ src/main/java/com/stockheap/weather/controller_advice/response/GenericWeatherResponse.java
@@ -0,0 +1,9 @@
+package com.stockheap.weather.controller_advice.response;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class GenericWeatherResponse {
+    private String message;
+
+}
