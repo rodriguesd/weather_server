dbc284824f368f39554bca4ad8be936f8cb04127
rodriguesd <rodriguesd@gmail.com>
2025-03-07 20:25:40 -0800

    more fixes

--- src/main/java/com/stockheap/weather/controller_advice/WeatherExceptionHandler.java
+++ src/main/java/com/stockheap/weather/controller_advice/WeatherExceptionHandler.java
@@ -3,16 +3,19 @@ package com.stockheap.weather.controller_advice;
 import com.stockheap.weather.controller_advice.response.GenericWeatherResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class WeatherExceptionHandler {
 
-    @ExceptionHandler({Exception.class})
-    public ResponseEntity<GenericWeatherResponse> weatherServerException(Exception exception) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new GenericWeatherResponse(exception.getMessage()));
+
+
+    @ExceptionHandler(Exception.class)
+    public String handleNotFound(Exception e, Model model) {
+        model.addAttribute("message", "The REST API you are looking for doesn't exist.");
+        return "error";
     }
 }
