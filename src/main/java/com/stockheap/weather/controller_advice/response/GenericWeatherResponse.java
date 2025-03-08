ab01f9752c77034f0a4dabb34b462dd3b39ff5f1
rodriguesd <rodriguesd@gmail.com>
2025-03-08 11:24:32 -0800

    remove code that is not used

--- src/main/java/com/stockheap/weather/controller_advice/response/GenericWeatherResponse.java
+++
@@ -1,9 +0,0 @@
-package com.stockheap.weather.controller_advice.response;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class GenericWeatherResponse {
-    private String message;
-
-}
